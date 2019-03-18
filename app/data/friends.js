bdfaf7df334ca16f2a70258644d7cb48c340a8d9
BZGitHub <bzmessages@gmail.com>
2019-03-17 21:59:19 -0600

    added files

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Mario",
+		photo: "http://vignette3.wikia.nocookie.net/fantendo/images/f/f6/MarioHand.png/revision/latest?cb=20110731140119",
+		scores: [1, 5, 1, 1, 3, 1, 5, 5, 5, 1]
+	},
+	{
+		name: "Nathan Drake",
+		photo: "http://www.thegeekgeneration.com/wp-content/uploads/2010/10/Nathan-Drake.jpg",
+		scores: [5, 5, 4, 5, 1, 3, 5, 5, 1, 4]
+	},
+	{
+		name: "Master Chief",
+		photo: "https://tse3.mm.bing.net/th?id=OIP.ULAHqjg6TqRQ32MIw_X8eAHaL_&pid=15.1&P=0&w=300&h=300",
+		scores: [5, 1, 5, 5, 5, 4, 1, 5, 1, 5]
+	},
+	{
+		name: "Aloy",
+		photo: "https://cdn.gamerant.com/wp-content/uploads/horizon-zero-dawn-aloy-sequel-738x410.jpg.optimal.jpg",
+		scores: [5, 1, 5, 1, 1, 3, 5, 5, 1, 5]
+	},
+	{
+		name: "Donkey Kong",
+		photo: "https://vinnylanni.files.wordpress.com/2012/04/dkjc1.jpg",
+		scores: [1, 3, 1, 1, 1, 5, 1, 3, 4, 1]
+	}
+];
+
+module.exports = friends;
